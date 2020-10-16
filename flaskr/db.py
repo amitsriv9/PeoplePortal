fc0d16aaab1d2580962d227bab32c6789636bb7c
Amit <amitsriv9@gmail.com>
2020-10-16 16:52:12 +0530

    Fix the module used for mysql connection

--- flaskr/db.py
+++ flaskr/db.py
@@ -1,13 +1,14 @@
-from flask import current_app,g
-from flaskext.mysql import MySQL
+from flask import app,g
+from flask_mysqldb import MySQL
 
 def init_db(app):
-    get_db(app)
-    mysql = MySql()
-    mysql.init_app(app)
+    app.config['MYSQL_USER']='devuser'
+    app.config['MYSQL_PASSWORD'] = 'devuser'
+    app.config['MYSQL_DB'] = 'bluedb'
+    app.config['MYSQL_HOST']='localhost'
+    mysql = MySQL(app)
+    #mysql.init_app(app)
+    return mysql
 
 def get_db(app):
-    app.config['MYSQL_DATABASE_USER']='notroot'
-    app.config['MYSQL_DATABASE_PASSWORD'] = 'noroot'
-    app.config['MYSQL_DATABASE_DB'] = 'someData'
-    app.config['MYSQL_DATABASE_HOST']='localhost'
+    return init_db(app)
